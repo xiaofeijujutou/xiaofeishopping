d2c6f8a3994692e39db4db19d151d00341ea6de3
xiaofeijujutou <h1305392100@gmail.com>
2023-08-16 18:31:17 +0800

    项目搭建各个模块,下一步是建表和逆向工程

---
+++ menber/src/test/java/com/xiaofei/menber/MenberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaofei.menber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
